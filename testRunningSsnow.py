9eaf1069f1a4f17b398d88326e26017ab52a9d56
anbarth <anna2nsb@gmail.com>
2021-03-07 11:31:28 -0800

    idk what i did to this but here u go

--- testRunningSsnow.py
+++ testRunningSsnow.py
@@ -1,4 +1,5 @@
 import numpy as np
+import random
 import csv
 import matplotlib.pyplot as plt
 import exclVol
@@ -19,18 +20,28 @@ for filename in os.listdir(dir):
             Sbead.append([float(row[0]),float(row[1])])
     
     for (S,psi6) in Sbead:
-        if psi6 >= 0.99:
+        if psi6 >= 0.98:
             Spsihigh.append(S)
         else:
             Smain.append(S)
     
     Ssnowrunning = []
-    for i in range(1,len(Spsihigh)):
-        Ssnowrunning.append(np.mean(Spsihigh[0:i]))
-    
+    j = random.randrange(0,len(Spsihigh))
+    Ssnowrunning.append(Spsihigh[j])
+    for i in range(399):
+        j = random.randrange(0,len(Spsihigh))
+        Ssnowrunning.append( (Ssnowrunning[i]*(i+1)+Spsihigh[j])/(i+2)  )
+    #print(len(Spsihigh))
+    #for i in range(1,len(Spsihigh)+1):
+    #    Ssnowrunning.append(np.mean(Spsihigh[0:i]))
+
+
     plt.clf()
     #plt.xlim(0,200)
-    plt.hlines(np.mean(Spsihigh),1,len(Spsihigh))
-    plt.plot([i for i in range(1,len(Spsihigh))],Ssnowrunning)
-    plt.savefig(filename[0:-4]+'_runningSnow99.png')
+    plt.hlines(np.mean(Spsihigh),1,400)
+    plt.hlines(np.mean(Spsihigh)*1.02,1,400,ls='--')
+    plt.hlines(np.mean(Spsihigh)*0.98,1,400,ls='--')
+    #plt.plot([i for i in range(1,201)],Ssnowrunning)
+    plt.plot([i for i in range(len(Ssnowrunning))],Ssnowrunning)
+    plt.savefig(filename[0:-4]+'_runningSnowRando.png')
 
