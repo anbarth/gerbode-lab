e317e6dde7525ac6a62243bcb8a50de9813d9ebf
anbarth <anna2nsb@gmail.com>
2021-03-22 18:03:45 -0700

    finding freespace via the polygon method seems to be up and running!

--- exclVol.py
+++ exclVol.py
@@ -9,6 +9,7 @@ import operator
 import os
 import multiprocessing as mp
 import PIL
+from matplotlib import path
 
 class PolycrystalGrid:
     # class fields:
@@ -97,6 +98,11 @@ class PolycrystalGrid:
 
                     self.neighbs[p] = myNeighbs
 
+
+    def polygonAt(self,p):
+        N = 10 # jus for now
+        delta = 2*np.pi/N
+        return [ ( p[0]+self.beadRad*np.cos(n*delta), p[1]+self.beadRad*np.sin(n*delta) ) for n in range(N) ]
 
     # sets the shape of a particle centered at (0,0), so you can translate it to any other position
     # ^ that's what pxOccupiedByParticle does
@@ -289,7 +295,6 @@ class PolycrystalGrid:
     def freeSpace(self,particleCenter):
         particle = self.pxOccupiedByParticle(particleCenter) # particle includes all (x,y) to ignore
         freePx = []
-        #pxToCheck = particle[:] # make a COPY!!!!
         pxToCheck = [particleCenter]
         for (x,y) in pxToCheck:
             if self.isAvailableIgnore((x,y),particle):
@@ -299,6 +304,49 @@ class PolycrystalGrid:
                     if neighbor not in pxToCheck:
                         pxToCheck.append(neighbor)
         return freePx
+
+    def isAvailPoly(self,oldCenter,newCenter):
+        nns = self.neighbs[oldCenter]
+        newPolyPath = path.Path(self.polygonAt(newCenter))
+        # loops over nearest neighbors
+        for nn in nns:
+            neighbPoly = self.polygonAt(nn)
+            # if they overlap, this spot is not available
+            overlapPts = newPolyPath.contains_points(neighbPoly)
+            if any(overlapPts):
+                return False
+        # made it through all the neighbors? you're good to go
+        return True
+
+
+    def freeSpacePoly(self,particleCenter):
+        freePx = []
+        pxToCheck = [particleCenter]
+        for (x,y) in pxToCheck:
+            if self.isAvailPoly(particleCenter,(x,y)):
+                freePx.append((x,y))
+                neighbors = self.getNeighbors((x,y))
+                for neighbor in neighbors:
+                    if neighbor not in pxToCheck:
+                        pxToCheck.append(neighbor)
+        return freePx
+
+
+
+    def showFreeSpacePoly(self,particleCenter):
+        freePx = self.freeSpacePoly(particleCenter)
+
+        # lots of copied-pasted from showGrid lol
+        fig, ax = plt.subplots()
+        for (x,y) in self.particleCenters:
+            circ = plt.Circle((x, y), self.beadRad, color='gray', alpha=0.3)
+            ax.add_artist(circ)
+
+        plt.scatter(*zip(*self.occupiedPx),marker='.')
+        plt.scatter(*zip(*freePx),marker='.',color='red')
+        plt.xlim(0,self.gridSize[0])
+        plt.ylim(0,self.gridSize[1])
+        plt.show()
 
     def showFreeSpace(self,particleCenter):
         freePx = self.freeSpace(particleCenter)
