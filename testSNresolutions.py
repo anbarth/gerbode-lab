45c25688af128f388790bb9b6c0ce5005c10c8a2
Anna Barth <anna2nsb@gmail.com>
2021-03-26 09:52:20 -0700

    idk what i did to testSNres but here. new file that does rs2 readshock analysis.

--- testSNresolutions.py
+++ testSNresolutions.py
@@ -5,19 +5,21 @@ import exclVol
 import importlib
 importlib.reload(exclVol)
 
+
 if __name__ == '__main__':
-    radii = [10,15,20,25,30,35]
-    with open('polyScalingMar24.csv','w',newline='') as file:
+    radii = [40,45]
+    with open('polyScalingMar25.csv','w',newline='') as file:
         writer = csv.writer(file)
         crysfilename = 'crystals/readshock2/rs7p5_1.csv'
         writer.writerow([crysfilename])
-        writer.writerow(['holy cow! polygon runtimes seem pretty good. lets see how high we can take the resolution! inclusive distance. 40 worker processes.'])
+        writer.writerow(['can we take resolution even higher?? inclusive distance. 40 worker processes.'])
         writer.writerow(['rad','N','S_old','runtime','S_poly','runtime'])
         
         for radius in radii:
             coll = exclVol.PolycrystalGrid(crysfilename,rad=radius,useNeighbs=True)
             print(coll.beadRad)
             (Sold,N1,Sbead1,told) =   coll.entropyParallel(40,makeImg=True,poly=False)
+            print("done with old freeSpace")
             (Spoly,N2,Sbead2,tpoly) = coll.entropyParallel(40,makeImg=True,poly=True)
             print(radius,N1,Sold,told,Spoly,tpoly)
             writer.writerow([radius,N1,Sold,told,Spoly,tpoly])
