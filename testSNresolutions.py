7866c366df070b5fc21359658e4a56c0c1ad94d0
Anna Barth <anna2nsb@gmail.com>
2021-03-28 16:27:34 -0700

    ready to compare the matplotlibless isAvailPoly to the old one!

--- testSNresolutions.py
+++ testSNresolutions.py
@@ -8,16 +8,16 @@ importlib.reload(exclVol)
 
 if __name__ == '__main__':
     radii = [r for r in range(5,48)]
-    with open('polyScalingMar25.csv','w',newline='') as file:
+    with open('polyScalingMar28.csv','w',newline='') as file:
         writer = csv.writer(file)
         crysfilename = 'crystals/readshock2/rs2p5_1.csv'
         writer.writerow([crysfilename])
-        writer.writerow(['freeSpacePoly, inclusive distance. how does S/N plateau? 40 worker processes. DrSpeedy'])
+        writer.writerow(['freeSpacePoly, inclusive distance. new isAvailPoly that doesnt ue matplotlib -- lets compare S/N and runtime! 40 worker processes. DrSpeedy'])
         writer.writerow(['rad','S_poly','N','runtime'])
         
         for radius in radii:
             coll = exclVol.PolycrystalGrid(crysfilename,rad=radius,useNeighbs=True)
             print(coll.beadRad)
-            (Spoly,N,Sbead,t) = coll.entropyParallel(40,makeImg=False,poly=True)
+            (Spoly,N,t) = coll.entropyParallel(40,makeImg=False,poly=True)
             print(radius,Spoly,N,t)
             writer.writerow([radius,Spoly,N,t])
