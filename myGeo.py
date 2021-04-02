9e124cf70fd3a3e15956bc4dab0d5190527c0ee1
anbarth <anna2nsb@gmail.com>
2021-04-02 12:09:18 -0700

    myGeo is a cookbook of geometry algorithms i found on stackoverflow

---
+++ myGeo.py
@@ -0,0 +1,46 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+# shoelace formula
+# stackoverflow.com/questions/24467972/calculate-area-of-polygon-given-x-y-coordinates
+def polyArea(corners):
+    n = len(corners) # of corners
+    area = 0.0
+    for i in range(n):
+        j = (i + 1) % n
+        area += corners[i][0] * corners[j][1]
+        area -= corners[j][0] * corners[i][1]
+    area = abs(area) / 2.0
+    return area
+
+# stackoverflow.com/questions/55816902/finding-the-intersection-of-two-circles
+# if no crossing points, returns None
+# if 2 crossing points, returns both in the form (x1,y1,x2,y2)
+# if the circles just kiss, returns the singular crossing point twice (x1,y1,x1,y1)
+def circIntersections(x0, y0, r0, x1, y1, r1):
+    # circle 1: (x0, y0), radius r0
+    # circle 2: (x1, y1), radius r1
+
+    d=np.sqrt((x1-x0)**2 + (y1-y0)**2)
+    
+    # non intersecting
+    if d > r0 + r1 :
+        return None
+    # One circle within other
+    if d < abs(r0-r1):
+        return None
+    # coincident circles
+    if d == 0 and r0 == r1:
+        return None
+    else:
+        a=(r0**2-r1**2+d**2)/(2*d)
+        h=np.sqrt(r0**2-a**2)
+        x2=x0+a*(x1-x0)/d   
+        y2=y0+a*(y1-y0)/d   
+        x3=x2+h*(y1-y0)/d     
+        y3=y2-h*(x1-x0)/d 
+
+        x4=x2-h*(y1-y0)/d
+        y4=y2+h*(x1-x0)/d
+        
+        return (x3, y3, x4, y4)
