45bc2c45afefc35a78bf5f1f2f5207dcbae6820e
anbarth <anna2nsb@gmail.com>
2021-04-11 17:41:58 -0700

    idk

--- testEntropyHisto.py
+++ testEntropyHisto.py
@@ -1,26 +1,24 @@
 import numpy as np
 import csv
 import matplotlib.pyplot as plt
-import exclVol
+import polycrystal
 import importlib
 import os
-importlib.reload(exclVol)
+importlib.reload(polycrystal)
 
 if __name__ == '__main__':
-    dir = r'C:\Users\GerbodeLab\Documents\banana\gerbode-lab\crystals\readshock2'
+    dir = r'C:\Users\anna2\OneDrive\Documents\Gerbode\python\crystals\readshock2'
     #dir = r'C:\Users\GerbodeLab\Documents\banana\gerbode-lab\readshock'
-    with open('readshock2_35px_excl_Entropiesmarch17.csv','w',newline='') as file:
+    with open('deleteme.csv','w',newline='') as file:
         writer = csv.writer(file)
-        writer.writerow(['readshock2 data at 35px, see also associated images! this time EXCLUSIVE dist!'])
+        writer.writerow(['readshock2 data, finally grid-free! see also associated images'])
         #writer.writerow(['readshock2 data at 5px, see also associated images!'])
         writer.writerow(['file','S','N','S/N','time'])
         for filename in os.listdir(dir):
             print("==================")
             print(filename)
-            coll = exclVol.PolycrystalGrid('crystals/readshock2/'+filename,rad=35,usePsi6=True)
-            #coll = exclVol.PolycrystalGrid(filename,rad=35)
-            print("n_bead: "+str(len(coll.beadShape)))
-            (S,numParts,Sbead,time) = coll.entropyParallel(40,makeImg=True)
-            print("time: "+str(time))
-            writer.writerow([filename,S,numParts,S/numParts])
+            coll = polycrystal.Polycrystal('crystals/readshock2/'+filename)
+            (S,numParts,time) = coll.entropyDraw()
+            #print("time: "+str(time))
+            #writer.writerow([filename,S,numParts,S/numParts,time])
     
